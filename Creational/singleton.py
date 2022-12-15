0156a63d06fdca51668500125af38190cc68d506
burchesoka <dvkotin@yandex.ru>
2022-12-15 18:28:04 +0500

    singelton upd

--- Creational/singleton.py
+++ Creational/singleton.py
@@ -5,41 +5,14 @@ class MetaSingleton(type):
         if cls not in cls._instances:
             cls._instances[cls] = super().__call__(*args, **kwargs)
 
-        print(cls._instances)
         return cls._instances[cls]
 
 
 class Logger(metaclass=MetaSingleton):
-    """  """
+    """ Single object e.g. database connection, logger etc. """
 
 
 if __name__ == "__main__":
     logger1 = Logger()
     logger2 = Logger()
-    print(logger1 is logger2)
-
-
-
-
-
-
-
-#
-#
-# class MetaSingleton(type):
-#     _instances = {}
-#
-#     def __call__(cls, *args, **kwargs):
-#         if cls not in cls._instances:
-#             cls._instances[cls] = super().__call__(*args, **kwargs)
-#         return cls._instances[cls]
-#
-#
-# class Logger(metaclass=MetaSingleton):
-#     pass
-#
-#
-# logger1 = Logger()
-# logger2 = Logger()
-#
-# print(logger1 is logger2)
+    assert logger1 is logger2
